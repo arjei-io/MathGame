da022f406d1a7bb3c347a84da9ee517eeb041a1e
arjei <214918003+arjei-io@users.noreply.github.com>
2026-02-20 14:41:57 +0200

    DivisionGame Method added
    Player can now play division game.

--- MathGame/ChooseGame.cs
+++ MathGame/ChooseGame.cs
@@ -92,7 +92,50 @@ namespace MathGame
 
         internal void DivisionGame()
         {
-            //TODO:
+            Random random = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                int num1 = random.Next(1, 100);
+                int num2 = random.Next(1, 100);
+
+               
+
+                while (num1 % num2 != 0)
+                {
+                    num1 = random.Next(1, 100);
+                    num2 = random.Next(1, 100);
+                }
+
+                int validNum1 = num1;
+                int validNum2 = num2;
+
+                Console.WriteLine($"What's {validNum1} / {validNum2}");
+                var result = Console.ReadLine();
+
+                while (string.IsNullOrEmpty(result) || !Int32.TryParse(result, out _))
+                {
+                    Console.WriteLine("Please enter an integer");
+                    result = Console.ReadLine();
+                }
+
+                if (int.Parse(result) == validNum1 / validNum2)
+                {
+                    Console.WriteLine("Correct! Press 'enter' to continue");
+                    Console.ReadLine();
+                }
+                else if (int.Parse(result) != validNum1 / validNum2)
+                {
+                    Console.WriteLine("Incorrect! Press 'enter' to continue");
+                    result = Console.ReadLine();
+                }
+
+                if (i == 4)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Thanks for playing. Press 'enter' to return to menu");
+                    Console.ReadLine();
+                }
+            }
         }
 
         internal void MultiplyGame()
